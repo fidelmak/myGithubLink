3b453471ca33c27471f0a565e61df3b06bc6e16d
Oluborode Akintunde Paul <64453973+fidelmak@users.noreply.github.com>
2023-09-12 12:40:37 +0100

    Please enter the commit message for your changes. Lines starting

---
+++ lib/tasks/web_view_container.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class WebViewContainer extends StatefulWidget {
+  // Specify the data type for the URL (String)
+
+  @override
+  _WebViewContainerState createState() => _WebViewContainerState();
+}
+
+class _WebViewContainerState extends State<WebViewContainer> {
+  final _key = UniqueKey();
+  //late WebViewController controller;
+
+  final controller = WebViewController()
+    ..setJavaScriptMode(JavaScriptMode.unrestricted)
+    ..setBackgroundColor(const Color(0x00000000))
+    ..setNavigationDelegate(
+      NavigationDelegate(
+        onProgress: (int progress) {
+          // Update loading bar.
+        },
+        onPageStarted: (String url) {},
+        onPageFinished: (String url) {},
+        onWebResourceError: (WebResourceError error) {},
+        onNavigationRequest: (NavigationRequest request) {
+          if (request.url.startsWith('https://www.github.com/fidelmak')) {
+            return NavigationDecision.prevent;
+          }
+          return NavigationDecision.navigate;
+        },
+      ),
+    )
+    ..loadRequest(Uri.parse('https://github.com/fidelmak'));
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.grey.shade400,
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: WebViewWidget(controller: controller),
+          ),
+        ],
+      ),
+    );
+  }
+}
